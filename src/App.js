93f70b75a34382726e642b956a02ad944e8a0037
Yohenba18 <roddik12kshetri@gmail.com>
2021-04-17 20:49:27 +0530

    tab info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+  
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
 
+const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [loading,setLoading] = useState(true);
+  const [jobs,setJobs] = useState([]);
+  const [value,setValue] = useState(0);
+
+  const fetchJobs = async() => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false)
+  }
+
+  useEffect(()=>{
+    fetchJobs();
+  },[])
+
+  if(loading){
+    return (
+      <section className="section loading">
+        Loading...
+      </section>
+    )
+  }
+
+  const {company,duties,dates,title} = jobs[value]
+  return <section className="section">
+    <div className="title">
+      <h2>experience</h2>
+      <div className="underline"></div>
     </div>
-  );
+    <div className="jobs-center">
+      {/*btn container */}
+      <article className="job-info">
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
+        {duties.map((duty) => {
+
+        })}
+        </article>
+    </div>
+  </section>
 }
 
-export default App;
+export default App
